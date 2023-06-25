8e4f5488c83e8959fb89e61f71f66b4cf29a6852
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2023-06-25 12:30:24 +0900

    refactor: reduce npm package size

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs'],
   minify: true,
-  sourcemap: true,
+  sourcemap: false, // reduce the npm package size by disabling sourcemaps (8MB -> 1MB)
   external: ['espree'],
 })
